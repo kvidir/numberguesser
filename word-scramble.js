77c8a36bb4b6b92dc4c00e19f764e4aaf31d036d
kvidir <126121160+kvidir@users.noreply.github.com>
2025-07-21 23:12:25 +0000

    Update word-scramble.js

--- word-scramble.js
+++ word-scramble.js
@@ -86,3 +86,11 @@ document.getElementById('next-word').addEventListener('click', pickNewWord);
 
 // Load first word
 pickNewWord();
+
+// Mobile navbar toggle
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('navbar-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
